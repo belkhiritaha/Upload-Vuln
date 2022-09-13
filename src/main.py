d41afa10e2c0719e10b4e42f600fba41eba777e0
Taha Belkhiri <belkhiri.taha53@gmail.com>
2022-09-13 22:22:36 +0200

    showing all img files in uploads/ on the HTML response

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from PIL import Image, ImageDraw, ImageFont
+from bs4 import BeautifulSoup
+import os
 
 app = Flask(__name__)
 
@@ -19,26 +21,60 @@ def insertWaterMark(filename):
     # Open the meme image
     img = Image.open(filename).convert("RGBA")
     # Open the watermark image
-    watermark = Image.open('download.jpeg').convert("RGBA")
+    watermark = Image.open('assets/watermark.jpeg').convert("RGBA")
     # Add the watermark to the image
     img.paste(watermark, (0, 0), watermark)
     # Save the image
-    newFilename = 'memes/ZZ' + filename
+    newFilename = filename.rsplit('/', 1)[1]
+    newFilename = 'memes/ZZ' + newFilename
     # Convert to RGB to avoid problems with non-PNG files
     img = img.convert("RGB")
     img.save(newFilename)
 
     print("New filename: ", newFilename)
-    return {'status': 'success', 'image': f'static/petpets/{newFilename}'}, 200
+    return {'status': 'success', 'image': f'memes/{newFilename}'}, 200
 
     # except:
     #     print("Error")
     #     return {'status': 'failed', 'message': 'Something went wrong'}, 500
 
 
+def getMemes():
+    memes = []
+    for filename in os.listdir('memes'):
+        if allowed_file(filename):
+            memes.append(filename)
+    return memes
+
+
+def editHTML(memes : list):
+    # Load file and parse with BeautifulSoup
+    html = open("src/index.html")
+    print("HTML loaded")
+    soup = BeautifulSoup(html, "html.parser")
+    print("HTML parsed")
+    for meme in memes:
+        # Create a new div
+        newDiv = soup.new_tag("div")
+        # Create a new img tag
+        newImg = soup.new_tag("img")
+        # Set the src of the img tag
+        newImg['src'] = f'memes/{meme}'
+        # Add the img tag to the div
+        newDiv.append(newImg)
+        # Add the div to the memes div
+        soup.find(id="memes").append(newDiv)
+        
+    # Write the new HTML to new file
+    with open("output.html", "w") as file:
+        file.write(str(soup))
+    
+
 @app.route('/')
-def hello():
-    insertWaterMark('download.jpeg')
-    return "Hello World!"
+def main():
+    insertWaterMark('uploads/download.jpeg')
+    memes = getMemes()
+    editHTML(memes)
+    return "h"
 
 
